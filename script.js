a1891ceed728544fbf75e0ac7510f0cd3da887b0
Wei-0222 <lu105205@apps.ntpc.edu.tw>
2025-07-04 11:07:00 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function sayHello() {
-  alert("你好，歡迎來到我的網站！");
+let votes = {
+  apple:0,
+  banan:0,
+  orange:0,
+};
+
+function vote(frit){
+  votes[furit]++;
+  updateResults();
+}
+
+function updateResults(){
+  document.getElementById("apple-count").textContent = votes.apple;
+  document.getElementById("banana-count").textContent = votes.banana;
+  document.getElementById("orange-count").textContent = votes.orange;
+  
 }
